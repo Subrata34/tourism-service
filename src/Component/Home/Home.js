b16ab04988399edbf9f5e411b520a5f4fa49226b
Subrata34 <subratamodak3491@gmail.com>
2022-01-10 23:58:34 +0600

    files

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap-v5'
+
+const Home = () => {
+    return (
+        <div>
+            <Button variant="success">firstly</Button>
+        </div>
+    )
+}
+
+export default Home
