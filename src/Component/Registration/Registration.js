24e5df0fe0b9901cbcf3534216b4e18c53bb0ee9
Subrata34 <subratamodak3491@gmail.com>
2022-01-11 08:20:24 +0600

    files

---
+++ src/Component/Registration/Registration.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Registration = () => {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">Name :</label>
+                <br />
+                <input type="text"  />
+                <br />
+                <label htmlFor="">Email:</label>
+                <br />
+                <input type="email" />
+                <br />
+                <label htmlFor="">Password :</label>
+                <br />
+                <input type="password" />
+                <br />
+                <label htmlFor="">Retype password :</label>
+                <br />
+                <input type="password" />
+            </form>
+        </div>
+    )
+}
+
+export default Registration
